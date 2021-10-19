fb5df9489ae108bfd4c1e282c8048dbe58337c4b
Saurabh singh rana <ranasaurabh455@gmail.com>
2021-10-19 10:47:13 +0530

    fix some bugs

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
-import National from '../component/National/National'
+
 const Header = () => {
   const { currentUser, logout } = useAuth();
   const [error, setError] = useState("");
